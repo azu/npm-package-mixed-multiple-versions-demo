436b38e83f814dc1efe82b329f4342163dd4cfb9
azu <azuciao@gmail.com>
2016-05-02 14:35:43 +0900

    add output

--- test.js
+++ test.js
@@ -4,5 +4,7 @@ var assert = require("assert");
 var lodash3 = require("lodash3");
 var lodash4 = require("lodash4");
 
+console.log(lodash3.VERSION);
 assert(lodash3.VERSION.includes("3"));
+console.log(lodash4.VERSION);
 assert(lodash4.VERSION.includes("4"));
